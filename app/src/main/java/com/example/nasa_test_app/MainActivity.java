299dfbd97b54d725d3f2375037733219cd06de64
Alex Nazarov <storyofspolding@gmail.com>
2021-02-03 22:11:19 +0300

    corrected switch

--- app/src/main/java/com/example/nasa_test_app/MainActivity.java
+++ app/src/main/java/com/example/nasa_test_app/MainActivity.java
@@ -7,8 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.CompoundButton;
+import android.view.View;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nasa_test_app.adapter.NasaAdapter;
@@ -17,7 +18,6 @@ import com.example.nasa_test_app.api.NetworkService;
 import com.example.nasa_test_app.data.Datum;
 import com.example.nasa_test_app.data.Item;
 import com.example.nasa_test_app.data.Link;
-import com.example.nasa_test_app.data.ObjectCollection;
 
 import java.util.ArrayList;
 
@@ -26,36 +26,54 @@ import java.util.List;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.BiConsumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private NasaAdapter adapter;
     private CompositeDisposable compositeDisposable;
+    private TextView textViewSpace;
+    private TextView textViewMars;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch switchNasa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Switch switch1 = findViewById(R.id.switch1);
+        textViewSpace = findViewById(R.id.textViewSpace);
+        textViewMars = findViewById(R.id.textViewMars);
+        switchNasa = findViewById(R.id.switchNasa);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         adapter = new NasaAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        switch1.setChecked(true);
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    loadMarsData();
-                } else {
-                    loadSpaceData();
-                }
-            }
-        });
-        switch1.setChecked(false);
+        switchNasa.setChecked(true);
+        switchNasa.setOnCheckedChangeListener((buttonView, isChecked) -> methodOfSwitch(isChecked));
+        switchNasa.setChecked(false);
+    }
+
+    private void methodOfSwitch(boolean isMars) {
+        if (isMars) {
+            loadMarsData();
+            textViewMars.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewSpace.setTextColor(getResources().getColor(R.color.colorWhite));
+        } else {
+            loadSpaceData();
+            textViewMars.setTextColor(getResources().getColor(R.color.colorWhite));
+            textViewSpace.setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
+
+    public void setSpace(View view) {
+        methodOfSwitch(false);
+        switchNasa.setChecked(false);
+    }
+
+    public void setMars(View view) {
+        methodOfSwitch(true);
+        switchNasa.setChecked(true);
     }
 
     private void imageClickListener() {
@@ -63,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             Link link1 = adapter.getLinkList().get(position);
             Datum datum1 = adapter.getDatumList().get(position);
             Intent intent = new Intent(MainActivity.this, NasaDetailActivity.class);
-            intent.putExtra("content", link1.getHref());
+            intent.putExtra("image", link1.getHref());
             intent.putExtra("title", datum1.getTitle());
             intent.putExtra("desc", datum1.getDescription());
             MainActivity.this.startActivity(intent);
@@ -77,39 +95,36 @@ public class MainActivity extends AppCompatActivity {
         Disposable disposable = api.getAllSpaceCollections()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new BiConsumer<ObjectCollection, Throwable>() {
-                    @Override
-                    public void accept(ObjectCollection objectCollection, Throwable throwable) throws Exception {
-                        if (throwable != null) {
-                            Toast.makeText(MainActivity.this, "Data loading error " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-                        } else {
-                            //Get All lists
-                            List<List<Link>> link = new ArrayList<>();
-                            List<List<Datum>> datum = new ArrayList<>();
-                            List<Link> linkList = new ArrayList<>();
-                            List<Datum> datumList = new ArrayList<>();
-                            List<Item> items = objectCollection.getCollection().getItems();
+                .subscribe((objectCollection, throwable) -> {
+                    if (throwable != null) {
+                        Toast.makeText(MainActivity.this, "Data loading error " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Get All lists
+                        List<List<Link>> link = new ArrayList<>();
+                        List<List<Datum>> datum = new ArrayList<>();
+                        List<Link> linkList = new ArrayList<>();
+                        List<Datum> datumList = new ArrayList<>();
+                        List<Item> items = objectCollection.getCollection().getItems();
 
-                            //Fill link and datum lists data from item list
-                            for (int i = 0; i < items.size(); i++) {
-                                link.add(objectCollection.getCollection().getItems().get(i).getLinks());
-                                datum.add(objectCollection.getCollection().getItems().get(i).getData());
-                            }
-                            //Get and add all dates to linkList
-                            for (int i = 0; i < link.size(); i++) {
-                                List<Link> links = link.get(i);
-                                linkList.addAll(links);
-                            }
-                            //Get and add all dates to datumList
-                            for (int i = 0; i < datum.size(); i++) {
-                                List<Datum> datumList1 = datum.get(i);
-                                datumList.addAll(datumList1);
-                            }
-                            adapter.setLinkList(linkList);
-                            adapter.setDatumList(datumList);
-
-                           imageClickListener();
+                        //Fill link and datum lists data from item list
+                        for (int i = 0; i < items.size(); i++) {
+                            link.add(objectCollection.getCollection().getItems().get(i).getLinks());
+                            datum.add(objectCollection.getCollection().getItems().get(i).getData());
                         }
+                        //Get and add all dates to linkList
+                        for (int i = 0; i < link.size(); i++) {
+                            List<Link> links = link.get(i);
+                            linkList.addAll(links);
+                        }
+                        //Get and add all dates to datumList
+                        for (int i = 0; i < datum.size(); i++) {
+                            List<Datum> datumList1 = datum.get(i);
+                            datumList.addAll(datumList1);
+                        }
+                        adapter.setLinkList(linkList);
+                        adapter.setDatumList(datumList);
+
+                       imageClickListener();
                     }
                 });
         compositeDisposable.add(disposable);
@@ -122,39 +137,36 @@ public class MainActivity extends AppCompatActivity {
         Disposable disposable = api.getAllMarsCollection()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new BiConsumer<ObjectCollection, Throwable>() {
-                    @Override
-                    public void accept(ObjectCollection objectCollection, Throwable throwable) throws Exception {
-                        if (throwable != null) {
-                            Toast.makeText(MainActivity.this, "Data loading error " + throwable, Toast.LENGTH_SHORT).show();
-                        } else {
-                            //Get All lists
-                            List<List<Link>> link = new ArrayList<>();
-                            List<List<Datum>> datum = new ArrayList<>();
-                            List<Link> linkList = new ArrayList<>();
-                            List<Datum> datumList = new ArrayList<>();
-                            List<Item> items = objectCollection.getCollection().getItems();
+                .subscribe((objectCollection, throwable) -> {
+                    if (throwable != null) {
+                        Toast.makeText(MainActivity.this, "Data loading error " + throwable, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Get All lists
+                        List<List<Link>> link = new ArrayList<>();
+                        List<List<Datum>> datum = new ArrayList<>();
+                        List<Link> linkList = new ArrayList<>();
+                        List<Datum> datumList = new ArrayList<>();
+                        List<Item> items = objectCollection.getCollection().getItems();
 
-                            //Fill link and datum lists data from item list
-                            for (int i = 0; i < items.size(); i++) {
-                                link.add(objectCollection.getCollection().getItems().get(i).getLinks());
-                                datum.add(objectCollection.getCollection().getItems().get(i).getData());
-                            }
-                            //Get and add all dates to linkList
-                            for (int i = 0; i < link.size(); i++) {
-                                List<Link> links = link.get(i);
-                                linkList.addAll(links);
-                            }
-                            //Get and add all dates to datumList
-                            for (int i = 0; i < datum.size(); i++) {
-                                List<Datum> datumList1 = datum.get(i);
-                                datumList.addAll(datumList1);
-                            }
-                            adapter.setLinkList(linkList);
-                            adapter.setDatumList(datumList);
-
-                            imageClickListener();
+                        //Fill link and datum lists data from item list
+                        for (int i = 0; i < items.size(); i++) {
+                            link.add(objectCollection.getCollection().getItems().get(i).getLinks());
+                            datum.add(objectCollection.getCollection().getItems().get(i).getData());
                         }
+                        //Get and add all dates to linkList
+                        for (int i = 0; i < link.size(); i++) {
+                            List<Link> links = link.get(i);
+                            linkList.addAll(links);
+                        }
+                        //Get and add all dates to datumList
+                        for (int i = 0; i < datum.size(); i++) {
+                            List<Datum> datumList1 = datum.get(i);
+                            datumList.addAll(datumList1);
+                        }
+                        adapter.setLinkList(linkList);
+                        adapter.setDatumList(datumList);
+
+                        imageClickListener();
                     }
                 });
         compositeDisposable.add(disposable);
--- app/src/main/java/com/example/nasa_test_app/NasaDetailActivity.java
+++ app/src/main/java/com/example/nasa_test_app/NasaDetailActivity.java
@@ -10,23 +10,18 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 public class NasaDetailActivity extends AppCompatActivity {
-
-    private ImageView imageViewToDetails;
-    private TextView textViewDescription;
-    private TextView textViewTitle;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nasa_detail);
-        imageViewToDetails = findViewById(R.id.imageViewToDetails);
-        textViewDescription = findViewById(R.id.textViewDescription);
-        textViewTitle = findViewById(R.id.textViewTitle);
+        ImageView imageViewToDetails = findViewById(R.id.imageViewToDetails);
+        TextView textViewDescription = findViewById(R.id.textViewDescription);
+        TextView textViewTitle = findViewById(R.id.textViewTitle);
 
         Intent intent = getIntent();
-        if (intent != null && intent.hasExtra("content") && intent.hasExtra("title") && intent.hasExtra("desc")) {
-            String image = intent.getStringExtra("content");
+        if (intent != null && intent.hasExtra("image") && intent.hasExtra("title") && intent.hasExtra("desc")) {
+            String image = intent.getStringExtra("image");
             String title = intent.getStringExtra("title");
             String desc = intent.getStringExtra("desc");
             Picasso.get().load(image).into(imageViewToDetails);
